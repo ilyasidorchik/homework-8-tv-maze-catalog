33151a4891475f6468b6ac2613939df376756327
Ilya Sidorchik <ilya@sidorchik.ru>
2019-08-16 18:33:47 +0300

    Adds AppRouter

--- src/components/AppRouter/AppRouter.js
+++ src/components/AppRouter/AppRouter.js
@@ -1,3 +1,25 @@
 // Реализуйте роутер
 // Вам нужно определить корневой роут, который будет вести на страницу поиска.
 // Роут шоу должен принимать id в параметрах.
+
+import React, { PureComponent } from 'react';
+import Search from '../Search';
+import ShowPage from '../ShowPage';
+import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
+import './AppRouter.css';
+
+class AppRouter extends PureComponent {
+    render() {
+        return (
+            <div className="App">
+                <Switch>
+                    <Route path="/" component={Search} exact />
+                    <Route path="/shows:id" component={ShowPage} />
+                    <Redirect to="/" />
+                </Switch>
+            </div>
+        );
+    }
+}
+
+export default withRouter(AppRouter);
